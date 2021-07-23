885f1c8ad8b441eb0942a12ea677255c2f2dbe9e
Edgar Ruiz Dorador <edgar.ruizdrd@uanl.edu.mx>
2021-07-23 14:06:23 -0500

    Package ready

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+    "Oscar",
+    "Ana",
+    "Nicolay",
+    "Yasica",
+    "Diego",
+    "Laura"
+];
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message)
+}
+
+module.exports = { randomMsg };
